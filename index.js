7f84c22b543a59c7d2f98080cef915e2c235f223
Opilane TTHK <opilane@tthk.ee>
2025-11-10 16:50:47 +0200

    people massiiv

--- index.js
+++ index.js
@@ -63,6 +63,65 @@ app.put('/thingamabobs/:id', (req, res) => {
     res.status(200).send(thingamabobs[index]);
 }); 
 
+const people = [
+    {id: 1, name: "asd", email: "robi111@gmail.com"},
+    {id: 2, name: "das asd", email: "robi222@gmail.com"},
+    {id: 3, name: "sadas", email: "robi333@gmail.com"},
+]
+
+app.get('/people', (req, res) => {res.send(people)})
+
+app.get('/people/:id', (req, res) => {
+    if (typeof people[req.params.id -1] === 'undefined') 
+        {
+            return res.status(404).send({error:"Object not found. Check your people id"})
+        }
+        res.send(people[req.params.id -1]);
+})
+
+app.post('/people', (req, res) => {
+    if(!req.body.name || !req.body.email) 
+    {
+        return res.status(400).send({error:"One or multiple parameters missing"})
+    }
+
+    let peopleThingy = {
+        id: people.length+1, 
+        email: req.body.email, 
+        name: req.body.name
+    }
+
+    people.push(peopleThingy)
+    res.status(201).location('localhost:8080/people/'+(people.length-1)).send(peopleThingy)
+})
+
+app.delete('/people/:id', (req, res) => {
+    if (typeof people[req.params.id -1] === 'undefined') 
+        {
+            return res.status(404).send({error:"Object not found. Check your thingamabobs id"})
+        }
+    people.splice(req.params.id -1,1)
+    res.status(204).send({error: "No content"})
+})
+
+app.put('/people/:id', (req, res) => { 
+    if (!req.body.name || !req.body.email) {
+        return res.status(400).send({ error: "One or multiple parameters missing" });
+    }
+    
+    const id = parseInt(req.params.id);
+
+    const index = people.findIndex(t => t.id === id);
+    if (index === -1) {
+        return res.status(404).send({ error: "People not found" });
+    }
+
+    people[index].name = req.body.name;
+    people[index].price = req.body.email;
+    res.status(200).send(people[index]);
+}); 
+
+
 app.listen(8080, () => {
     console.log(`API running at: http://localhost:8080`)
 })
